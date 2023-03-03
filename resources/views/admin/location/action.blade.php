5a20fe580365092a69e1796919dcd7c0d9e1c391
Sukanta Purkayastha <spurkayastha83@gmail.com>
2023-03-04 03:49:05 +0600

    location table created

--- resources/views/admin/location/action.blade.php
+++ resources/views/admin/location/action.blade.php
@@ -1,3 +1,56 @@
 @section('box')
-
+<!-- Create Modal-->
+<div class="modal fade" id="createModal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Add Location</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="" method="POST">
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-12 mb-3">
+                            <label
+                                for="name"
+                                class="form-label">
+                                Location
+                            </label>
+                            <input type="text" class="form-control" id="location">
+                        </div>
+                        <div class="col-6">
+                            <label
+                                for="latitude"
+                                class="form-label">
+                                Latitude
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Latitude"
+                                class="form-control"
+                                id="latitude">
+                        </div>
+                        <div class="col-6">
+                            <label
+                                for="longitude"
+                                class="form-label">
+                                Longitude
+                            </label>
+                            <input
+                                type="text"
+                                class="form-control"
+                                placeholder="Longitude"
+                                id="longitude">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-sm btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-sm btn-primary">Save Location</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- /Create Modal-->
 @endsection
--- resources/views/admin/location/location.blade.php
+++ resources/views/admin/location/location.blade.php
@@ -9,6 +9,8 @@
                         <span class="card-title">Locations</span>
                         <button
                             type="button"
+                            data-bs-toggle="modal"
+                            data-bs-target="#createModal"
                             class="btn btn-sm btn-outline-primary float-end">
                             <i class="bi bi-plus-circle-dotted"></i>
                             Add Location
@@ -20,48 +22,31 @@
                             <thead>
                             <tr>
                                 <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Position</th>
-                                <th scope="col">Age</th>
-                                <th scope="col">Start Date</th>
+                                <th scope="col">Location</th>
+                                <th scope="col">Latitude</th>
+                                <th scope="col">Longitude</th>
+                                <th scope="col"></th>
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Brandon Jacob</td>
-                                <td>Designer</td>
-                                <td>28</td>
-                                <td>2016-05-25</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Bridie Kessler</td>
-                                <td>Developer</td>
-                                <td>35</td>
-                                <td>2014-12-05</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Ashleigh Langosh</td>
-                                <td>Finance</td>
-                                <td>45</td>
-                                <td>2011-08-12</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">4</th>
-                                <td>Angus Grady</td>
-                                <td>HR</td>
-                                <td>34</td>
-                                <td>2012-06-11</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">5</th>
-                                <td>Raheem Lehner</td>
-                                <td>Dynamic Division Officer</td>
-                                <td>47</td>
-                                <td>2011-04-19</td>
-                            </tr>
+                                @foreach($locations as $key => $location)
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>{{ $location->name }}</td>
+                                        <td>{{ $location->latitude }}</td>
+                                        <td>{{ $location->longitude }}</td>
+                                        <td>
+                                            <button
+                                                class="btn btn-sm btn-outline-primary">
+                                                <i class="bi bi-pencil"></i>
+                                            </button>
+                                            <button
+                                                class="btn btn-sm btn-outline-danger">
+                                                <i class="bi bi-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                         <!-- End Table with stripped rows -->
