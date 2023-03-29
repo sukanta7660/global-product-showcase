41a6dff0c1e7e27ada36d82b95ffb2104a3a91bd
Sukanta Purkayastha <spurkayastha83@gmail.com>
2023-03-29 19:14:09 +0600

    add product recent review expiry date

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user()->role !== 'admin') {
+            return redirect('/');
+        }
+        return $next($request);
+    }
+}
